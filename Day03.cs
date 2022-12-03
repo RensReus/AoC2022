294a7f7d57e1ac50418fc900c188f3cfc258207c
RensReus <rensreus@gmail.com>
2022-12-03 13:44:52 +0100

    Update after learning about Intersect

--- Day03.cs
+++ Day03.cs
@@ -5,13 +5,14 @@ namespace AoC2022.Days03;
 class Day : BaseDay
 {
     static List<string> ProcessInput(bool part2, string suffix)
-    {
-        var readFile = ReadFile("03/" + suffix);
-        return readFile.Select(line => line).ToList();
-    }
+        => ReadFile("03/" + suffix).ToList();
 
     public override int Part1(string suffix)
-        => ProcessInput(false, suffix).Sum(x => PriorityValue(x));
+        => ProcessInput(false, suffix)
+            .Select(line => line.Substring(0, line.Length / 2)
+                .Intersect(line.Substring(line.Length / 2))
+                .First())
+                .Sum(x => x.PriorityValue());
 
     public override int Part2(string suffix)
     {
@@ -19,44 +20,22 @@ class Day : BaseDay
         var answer = 0;
         for (int i = 0; i < input.Count; i += 3)
         {
-            foreach (var letter in input.ElementAt(i))
-            {
-                if (input.ElementAt(i + 1).Contains(letter) && input.ElementAt(i + 2).Contains(letter))
-                {
-                    answer += PriorityValue(letter);
-                    break;
-                }
-            }
+            answer += input[i].Intersect(input[i + 1]).Intersect(input[i + 2]).First().PriorityValue();
         }
-
         return answer;
     }
-
-    private int PriorityValue(string line)
-    {
-        var A = line.Substring(0, line.Length / 2).ToCharArray();
-        var B = line.Substring(line.Length / 2).ToCharArray();
-        List<Char> Double = new List<Char>();
-        foreach (var letter in A)
-        {
-            if (B.Contains(letter) && !Double.Contains(letter))
-            {
-                Double.Add(letter);
-            }
-        }
-        return Double.Sum(letter => PriorityValue(letter));
-    }
-
-    private int PriorityValue(char letter)
-        => letter switch
-        {
-            <= 'Z' => letter - 'A' + 27,
-            >= 'a' => letter - 'a' + 1,
-            _ => throw new ArgumentOutOfRangeException()
-        };
 
     public override List<Case> Part1Cases() => new() { new("1a", 157), new("p1", 8298) };
 
     public override List<Case> Part2Cases() => new() { new("1a", 70), new("p1", 2708) };
 }
 
+public static class CharExtensions
+{
+    public static int PriorityValue(this char letter)
+        => letter switch
+        {
+            <= 'Z' => letter - 'A' + 27,
+            _ => letter - 'a' + 1,
+        };
+}
