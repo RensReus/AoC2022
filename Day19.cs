4291b7f751bf09ccef2650ad3713ae260025952e
Rens Reus <rensreus@gmail.com>
2022-12-18 23:54:09 +0100

    prep 19

---
+++ Day19.cs
@@ -0,0 +1,25 @@
+namespace AoC2022;
+
+static class Day19
+{
+    static IList<string> ProcessInput(string input)
+    {
+        return input.Split(";").ToList();
+    }
+
+    [Example(expected: 1111111, input: "AAAAA")]
+    [Puzzle(expected: 222222)]
+    public static int Part1(string input)
+    {
+        var processedInput = ProcessInput(input);
+        return 1111111;
+    }
+
+    [Example(expected: 1111111, input: "AAAAA")]
+    [Puzzle(expected: 222222)]
+    public static int Part2(string input)
+    {
+        var processedInput = ProcessInput(input);
+        return 1111111;
+    }
+}
