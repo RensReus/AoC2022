7179743682d00bf657fc9ed1b8b7ef254c9e56da
Rens Reus <r.reus@tjip.com>
2023-12-13 13:27:44 +0100

    Day13 cleanup

--- 2023/Solutions/Day13.cs
+++ 2023/Solutions/Day13.cs
@@ -1,5 +1,3 @@
-
-
 namespace AoC2023;
 
 class Day13 : BaseDay
@@ -7,36 +5,28 @@ class Day13 : BaseDay
     [Example(expected: 405, input: "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#")]
     [Puzzle(expected: 28651)] // 16117, 17509 too low, 30691 too high
     public static int Part1(string input)
+        => input.Split("\n\n").Sum(block => MirrorLineScore(block, MirrorLine));
+
+    private static int MirrorLineScore(string block, Func<List<string>, int, bool> IsMirrorLine)
     {
-        var blocks = input.Split("\n\n");
-        var answer = 0;
-        foreach (var block in blocks)
+        var lines = ReadLines(block);
+        for (int i = 1; i < lines.Count; i++)
         {
-            var lines = ReadLines(block);
-            for (int i = 1; i < lines.Count; i++)
-            {
-                if (MirrorLine(lines, i))
-                {
-                    answer += i * 100;
-                }
-            }
-
-            var verticalLines = new List<string>();
-            for (int i = 0; i < lines[0].Length; i++)
-            {
-                var newline = string.Concat(lines.Select(line => line[i]));
-                verticalLines.Add(newline);
-            }
-
-            for (int i = 1; i < verticalLines.Count; i++)
-            {
-                if (MirrorLine(verticalLines, i))
-                {
-                    answer += i;
-                }
-            }
+            if (IsMirrorLine(lines, i)) return i * 100;
         }
-        return answer;
+
+        var verticalLines = new List<string>();
+        for (int i = 0; i < lines[0].Length; i++)
+        {
+            var newline = string.Concat(lines.Select(line => line[i]));
+            verticalLines.Add(newline);
+        }
+
+        for (int i = 1; i < verticalLines.Count; i++)
+        {
+            if (IsMirrorLine(verticalLines, i)) return i;
+        }
+        return 0;
     }
 
     private static bool MirrorLine(List<string> lines, int line)
@@ -48,6 +38,11 @@ class Day13 : BaseDay
         }
         return true;
     }
+
+    [Example(expected: 400, input: "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#")]
+    [Puzzle(expected: 25450)]
+    public static int Part2(string input)
+        => input.Split("\n\n").Sum(block => MirrorLineScore(block, MirrorLineSmudge));
 
     private static bool MirrorLineSmudge(List<string> lines, int line)
     {
@@ -65,46 +60,8 @@ class Day13 : BaseDay
         var diff = 0;
         for (int i = 0; i < v1.Length; i++)
         {
-            if (v1[i] != v2[i])
-            {
-                diff++;
-            }
+            if (v1[i] != v2[i]) diff++;
         }
         return diff;
-    }
-
-    [Example(expected: 400, input: "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#")]
-    [Puzzle(expected: 25450)]
-    public static int Part2(string input)
-    {
-        var blocks = input.Split("\n\n");
-        var answer = 0;
-        foreach (var block in blocks)
-        {
-            var lines = ReadLines(block);
-            for (int i = 1; i < lines.Count; i++)
-            {
-                if (MirrorLineSmudge(lines, i))
-                {
-                    answer += i * 100;
-                }
-            }
-            var verticalLines = new List<string>();
-            for (int i = 0; i < lines[0].Length; i++)
-            {
-                var newline = string.Concat(lines.Select(line => line[i]));
-                verticalLines.Add(newline);
-            }
-
-            for (int i = 1; i < verticalLines.Count; i++)
-            {
-                if (MirrorLineSmudge(verticalLines, i))
-                {
-                    answer += i;
-                    Console.WriteLine("vert " + i);
-                }
-            }
-        }
-        return answer;
     }
 }
