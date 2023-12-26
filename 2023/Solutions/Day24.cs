292055b0963cbe0250fc244d5084ef50dddc15ee
Rens Reus <r.reus@tjip.com>
2023-12-26 13:28:39 +0100

    wat werk voor dag 24

--- 2023/Solutions/Day24.cs
+++ 2023/Solutions/Day24.cs
@@ -8,7 +8,7 @@ class Day24 : BaseDay
     [Puzzle(expected: 25810)]
     public static int Part1(string input)
     {
-        var hailstones = ReadLines(input).Select(x => new Hailstone1(x)).ToList();
+        var hailstones = ReadLines(input).Select(x => new Hailstone(x)).ToList();
         var answer = 0;
         for (int i = 0; i < hailstones.Count; i++)
         {
@@ -20,7 +20,7 @@ class Day24 : BaseDay
         return answer;
     }
 
-    private static (decimal, decimal, bool) Intersection(Hailstone1 one, Hailstone1 two)
+    private static (decimal, decimal, bool) Intersection(Hailstone one, Hailstone two)
     {
         //long minArea = 7;
         //long maxArea = 27;
@@ -40,73 +40,45 @@ class Day24 : BaseDay
     [Puzzle(expected: 222222)]
     public static long Part2(string input)
     {
-        var hailstones = ReadLines(input).Select(x => new Hailstone1(x)).ToList();
-        var dxMax = long.MaxValue;
-        var dyMax = long.MaxValue;
-        var dzMax = long.MaxValue;
-        var dxMin = long.MinValue;
-        var dyMin = long.MinValue;
-        var dzMin = long.MinValue;
-        var dxOverlaps = new Dictionary<long, List<Hailstone1>>();
+        var hailstones = ReadLines(input).Select(x => new Hailstone(x)).ToList();
+        var lowlim = long.MinValue;
+        var uplim = long.MaxValue;
+        var validRanges = new List<(long, long)>();
         for (int i = 0; i < hailstones.Count; i++)
         {
             for (int j = i + 1; j < hailstones.Count; j++)
             {
                 if (hailstones[i].Dx == hailstones[j].Dx)
                 {
-                    long newMax = (long)(Math.Abs(hailstones[i].Px - hailstones[j].Px) + Math.Abs(hailstones[j].Dx));
-                    dxMax = long.Min(dxMax, newMax);
-                    long newMin = (long)Math.Abs(hailstones[j].Dx);
-                    dxMin = long.Max(dxMin, newMin);
-                    Console.WriteLine($"x={hailstones[i].Dx}t+{hailstones[i].Px}");
-                    Console.WriteLine($"x={hailstones[j].Dx}t+{hailstones[j].Px}");
+                    var dx = (long)hailstones[j].Dx;
+                    long dp = (long)Math.Abs(hailstones[i].Px - hailstones[j].Px);
+                    validRanges.Add((dp, dx));
+                    lowlim = long.Max(lowlim, -dp + dx);
+                    uplim = long.Min(uplim, dp + dx);
                 }
-                if (hailstones[i].Dy == hailstones[j].Dy)
-                {
-                    long newMax = (long)(Math.Abs(hailstones[i].Py - hailstones[j].Py) + Math.Abs(hailstones[j].Dy));
-                    dyMax = long.Min(dyMax, newMax);
-                    long newMin = (long)Math.Abs(hailstones[j].Dy);
-                    dyMin = long.Max(dyMin, newMin);
-                    // Console.WriteLine($"dy {hailstones[i].Py - hailstones[j].Py}");
-                }
-                if (hailstones[i].Dz == hailstones[j].Dz)
-                {
-                    long newMax = (long)(Math.Abs(hailstones[i].Pz - hailstones[j].Pz) + Math.Abs(hailstones[j].Dz));
-                    dzMax = long.Min(dzMax, newMax);
-                    long newMin = (long)Math.Abs(hailstones[j].Dz);
-                    dzMin = long.Max(dzMin, newMin);
-                    // Console.WriteLine($"dz {hailstones[i].Pz - hailstones[j].Pz}");
-                }
-                // Console.WriteLine(Dpt(hailstones[i], hailstones[j]));
             }
         }
-        Console.WriteLine(dxMax);
-        Console.WriteLine(dyMax);
-        Console.WriteLine(dzMax);
-        // 
-        // Console.WriteLine(dxMin); // misschien niet geldig
-        // Console.WriteLine(dyMin);
-        // Console.WriteLine(dzMin);
-        return dxMax * 2 + dyMax * 2 + dzMax * 2;
-    }
+        validRanges = validRanges.OrderBy(x => x.Item2).ToList();
 
-    private static List<decimal> Divisors(List<decimal> dxLimits)
-    {
-        var response = new List<decimal>();
-        var limit = dxLimits.Max();
-        for (int i = 1; i <= limit; i++)
+        foreach (var range in validRanges[0..1])
         {
-            if (dxLimits.All(x => x % i == 0)) response.Add(i);
+            for (long i = range.Item2 + range.Item1; i >= range.Item2 - range.Item1; i += range.Item2)
+            {
+                var a = 1;
+                // Console.Write(i + ", ");
+            }
+            // Console.WriteLine();
         }
-        return response;
+        var combinedRange = validRanges[0];
+        for (int i = 1; i < validRanges.Count; i++)
+        {
+
+        }
+
+        return 0;
     }
 
-    private static string Dpt(Hailstone1 a, Hailstone1 b)
-    {
-        return $"({a.Px - b.Px}, {a.Py - b.Py}, {a.Pz - b.Pz}) + ({a.Dx - b.Dx}, {a.Dy - b.Dy}, {a.Dz - b.Dz})*t ";
-    }
-
-    private class Hailstone1
+    private class Hailstone
     {
         public decimal Px;
         public decimal Py;
@@ -115,7 +87,7 @@ class Day24 : BaseDay
         public decimal Dy;
         public decimal Dz;
 
-        public Hailstone1(string line)
+        public Hailstone(string line)
         {
             var p = line.Split(" @ ")[0].Split(", ");
             Px = decimal.Parse(p[0]);
@@ -132,33 +104,6 @@ class Day24 : BaseDay
             var xFuture = Dx > 0 ? xIntersect >= Px : xIntersect <= Px;
             var yFuture = Dy > 0 ? yIntersect >= Py : yIntersect <= Py;
             return xFuture && yFuture;
-        }
-
-        public override string ToString()
-        {
-            return $"{Px}, {Py}, {Pz} @ {Dx}, {Dy}, {Dz}";
-        }
-    }
-
-    private class Hailstone2
-    {
-        public long Px;
-        public long Py;
-        public long Pz;
-        public long Dx;
-        public long Dy;
-        public long Dz;
-
-        public Hailstone2(string line)
-        {
-            var p = line.Split(" @ ")[0].Split(", ");
-            Px = long.Parse(p[0]);
-            Py = long.Parse(p[1]);
-            Pz = long.Parse(p[2]);
-            var d = line.Split(" @ ")[1].Split(", ");
-            Dx = long.Parse(d[0]);
-            Dy = long.Parse(d[1]);
-            Dz = long.Parse(d[2]);
         }
 
         public override string ToString()
