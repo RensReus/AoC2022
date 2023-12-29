13b75e31ed28769c02e999f79291b0c652cdd6ac
Rens Reus <rensreus@gmail.com>
2023-12-29 22:30:29 +0100

    2022 day 19 part1

--- 2022/Solutions/Day19.cs
+++ 2022/Solutions/Day19.cs
@@ -1,67 +1,109 @@
-using System.Text.RegularExpressions;
-
 namespace AoC2022;
 
 class Day19 : BaseDay
 {
-    static IList<BluePrint> ProcessInput(string input)
+    private static List<BluePrint> ProcessInput(string input)
     {
         return input.Split("\n").Select(x => new BluePrint(x)).ToList();
     }
 
+    [Puzzle(expected: 1389)]
     [Example(expected: 33, input: "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.\nBlueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.")]
-    [Puzzle(expected: 222222)]
     public static int Part1(string input)
     {
         var bluePrints = ProcessInput(input);
-        var mostGeode = bluePrints.Select(MostGeodes).ToArray();
+        var mostGeode = bluePrints.Select(x => MostGeodes(x, 24));
         return mostGeode.Select((x, i) => x * (i + 1)).Sum();
     }
 
-    private static int MostGeodes(BluePrint bluePrint)
+    private static int MostGeodes(BluePrint bluePrint, int maxMinutes)
     {
-        var currStates = new HashSet<BPState> { new BPState() };
+        var currStates = new HashSet<BPState> { new() };
         var nextStates = new HashSet<BPState>();
-        for (int minute = 1; minute < 24; minute++)
+        var start = DateTime.Now;
+        for (int minute = 1; minute < maxMinutes; minute++)
         {
+            if ((DateTime.Now - start).TotalSeconds > 30)
+            {
+                Console.WriteLine("TIME TIME TIME");
+                break;
+            }
+            if (currStates.Count > 1_000_000 && minute != maxMinutes - 1)
+            {
+                Console.WriteLine($"{minute}, {currStates.Count}");
+                Console.WriteLine("COUNT COUNT COUNT");
+                break;
+            }
+
+            //if (currStates.Count > 10)
+            //{
+            //    Console.WriteLine($"{minute}, {nextStates.Count}");
+
+            //}
             foreach (var state in currStates)
             {
-                nextStates.UnionWith(GetNextStates(state, bluePrint));
+                var n = GetNextStates(state, bluePrint);
+                nextStates.UnionWith(n);
             }
-            if (nextStates.Count > 1_000_000)
-            {
-                Console.WriteLine("iets");
-            }
-            currStates = nextStates;
-            nextStates = new HashSet<BPState>();
+
+            currStates = Filter(nextStates);
+            nextStates = [];
         }
         currStates = currStates.Select(state => state.IncrementResources()).ToHashSet();
         var max = currStates.Max(state => state.Resources["geode"]);
+        Console.WriteLine($"\nDone, {currStates.Count}");
+        Console.WriteLine("max " + max);
+
         var bestStates = currStates.Where(state => state.Resources["geode"] == max);
         return max;
+    }
+
+    private static HashSet<BPState> Filter(HashSet<BPState> nextStates)
+    {
+        // dit maar efficienter
+        var filtered = new HashSet<BPState>();
+        foreach (var it in nextStates)
+        {
+            if (!filtered.Any(x => XBetter(x, it))) filtered.Add(it);
+        }
+        return filtered;
+    }
+
+    private static bool XBetter(BPState x, BPState value)
+    {
+        foreach (var item in x.Resources)
+        {
+            if (item.Value < value.Resources[item.Key]) return false;
+        }
+        foreach (var item in x.Robots)
+        {
+            if (item.Value < value.Robots[item.Key]) return false;
+        }
+        return true;
     }
 
     private static HashSet<BPState> GetNextStates(BPState state, BluePrint bluePrint)
     {
         var possibleRobots = bluePrint.RobotCosts.Where(robot => state.EnoughResources(robot.Value) && state.NeedsRobot(robot.Key, bluePrint));
         var newStates = possibleRobots.Select(state.BuildRobot).ToHashSet();
-        newStates.Add(state.IncrementResources());
+        if (state.DoNothingIsUsefull(bluePrint)) newStates.Add(state.IncrementResources());
         return newStates;
     }
 
-    [Example(expected: 1111111, input: "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.\nBlueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.")]
+    [Example(expected: 3472, input: "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.\nBlueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.")]
     [Puzzle(expected: 222222)]
     public static int Part2(string input)
     {
-        var processedInput = ProcessInput(input);
-        return 1111111;
+        var bluePrints = ProcessInput(input);
+        var mostGeode = bluePrints.Take(3).Select(x => MostGeodes(x, 32));
+        return mostGeode.Aggregate((x, y) => x * y);
     }
 }
 
 internal class BPState
 {
-    public Dictionary<string, int> Resources = new Dictionary<string, int> { { "ore", 0 }, { "clay", 0 }, { "obsidian", 0 }, { "geode", 0 } };
-    public Dictionary<string, int> Robots = new Dictionary<string, int> { { "ore", 1 }, { "clay", 0 }, { "obsidian", 0 }, { "geode", 0 } };
+    public Dictionary<string, int> Resources = new() { { "ore", 0 }, { "clay", 0 }, { "obsidian", 0 }, { "geode", 0 } };
+    public Dictionary<string, int> Robots = new() { { "ore", 1 }, { "clay", 0 }, { "obsidian", 0 }, { "geode", 0 } };
 
     public override int GetHashCode()
     {
@@ -96,9 +138,11 @@ internal class BPState
 
     private BPState Copy()
     {
-        var newState = new BPState();
-        newState.Resources = Resources.ToDictionary(entry => entry.Key, entry => entry.Value);
-        newState.Robots = Robots.ToDictionary(entry => entry.Key, entry => entry.Value);
+        var newState = new BPState
+        {
+            Resources = Resources.ToDictionary(entry => entry.Key, entry => entry.Value),
+            Robots = Robots.ToDictionary(entry => entry.Key, entry => entry.Value)
+        };
         return newState;
     }
 
@@ -124,7 +168,7 @@ internal class BPState
     // huidige productie / recipe -> x bots per turn
     private bool NeedsOreBot(BluePrint bluePrint)
     {
-        return Robots["ore"] < 2;
+        return Robots["ore"] < bluePrint.MaxOreBots;
     }
 
     private bool NeedsClayBot(BluePrint bluePrint)
@@ -145,6 +189,20 @@ internal class BPState
     {
         return !EnoughResources(bluePrint.RobotCosts["geode"]);
     }
+
+    public override string ToString()
+    {
+        return string.Join(",", Resources.Values) + " " + string.Join(",", Robots.Values);
+    }
+
+    internal bool DoNothingIsUsefull(BluePrint bp)
+    {
+        foreach (var cost in bp.RobotCosts.Values)
+        {
+            return cost.Any(cost2 => Resources[cost2.Key] < cost2.Value);
+        }
+        return true;
+    }
 }
 
 internal class BluePrint
@@ -162,8 +220,8 @@ internal class BluePrint
         RobotCosts["obsidian"] = new Dictionary<string, int> { { "ore", Int32.Parse(groups[3].Value) }, { "clay", Int32.Parse(groups[4].Value) } };
         RobotCosts["geode"] = new Dictionary<string, int> { { "ore", Int32.Parse(groups[5].Value) }, { "obsidian", Int32.Parse(groups[6].Value) } };
         MaxOreBots = RobotCosts.Max(x => x.Value["ore"]);
-        MaxClayBots = Int32.Parse(groups[4].Value) / Int32.Parse(groups[3].Value); // round up
-        MaxObsidianBots = Int32.Parse(groups[6].Value) / Int32.Parse(groups[5].Value);  // round up
+        MaxClayBots = Int32.Parse(groups[4].Value);
+        MaxObsidianBots = Int32.Parse(groups[6].Value);
     }
 
 }
