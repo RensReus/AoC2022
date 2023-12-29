d803a4806a2537b9735d11576e0788f4f01ff1e3
RensReus <rensreus@gmail.com>
2023-12-29 13:28:17 +0100

    Day12 2023 (#2)
    * some notes and ideas maar nog vast
    
    * some steps
    
    * ff een commit
    
    * Commit om terug te kijken
    
    * Day 12 part 2

--- 2023/Solutions/Day12.cs
+++ 2023/Solutions/Day12.cs
@@ -1,153 +1,146 @@
-// using System.Text;
 
-// namespace AoC2023;
+namespace AoC2023;
 
-// class Day12 : BaseDay
-// {
-//     [Example(expected: 21, input: "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1")]
-//     [Example(expected: 4, input: ".??..??...?##. 1,1,3")]
-//     [Example(expected: 4, input: "??.#??#?????##? 2,1,3,4")]
-//     [Puzzle(expected: 7541)]
-//     public static int Part1(string input)
-//         => ReadLines(input).Sum(CalculatePermutations);
+class Day12 : BaseDay
+{
+    private readonly Dictionary<string, long> Cache = [];
 
-//     private static int CalculatePermutations(string line)
-//     {
-//         var (springs, config) = (line.Split(" ")[0], line.Split(" ")[1].Split(',').Select(int.Parse).ToList());
-//         while (springs.Contains("..")) springs = springs.Replace("..", ".");
-//         springs = springs.Trim('.');
-//         Console.WriteLine(line);
-//         var answer = CalculatePermutations(springs, config);
-//         Console.WriteLine(answer);
-//         return answer;
-//     }
+    [Example(expected: 21, input: "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1")]
+    [Puzzle(expected: 7541)]
+    public long Part1(string input)
+    {
+        var answer = 0L;
+        var items = ReadLines(input).Select(x => ParseInput(x, false)).ToList();
 
-//     private static int CalculatePermutations(string springs, List<int> config)
-//     {
-//         var totalHekje = config.Sum();
-//         var totalOpen = springs.Length - totalHekje;
-//         var totalVariable = springs.Length - totalHekje - config.Count + 1;
-//         if (totalVariable == 0) return 1;
-//         while (true)
-//         {
-//             var newSprings = ReplaceSureFields(springs, config, totalHekje, totalOpen, totalVariable);
-//             (newSprings, config) = RemoveCompletedBlocks(newSprings, config);
+        foreach (var item in items)
+        {
+            var count = BruteForce(item.Item1, item.Item2);
+            Console.WriteLine(count + " " + item.Item1);
+            answer += count;
+        }
+        return answer;
+    }
 
-//             if (newSprings == springs) break;
-//             springs = newSprings;
-//             if (!springs.Contains('?')) return IsValid(springs, config) ? 1 : 0;
-//         }
+    [Example(expected: 525152, input: "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1")]
+    [Puzzle(expected: 17485169859432)]
+    public long Part2(string input)
+    {
+        var answer = 0L;
+        var items = ReadLines(input).Select(x => ParseInput(x, true)).ToList();
 
-//         if (!springs.Contains('?')) return IsValid(springs, config) ? 1 : 0;
-//         if (PrematureInvalid(springs, config)) return 0;
-//         var regex = new Regex(Regex.Escape("?"));
-//         var permutations = new string[] { regex.Replace(springs.ToString(), "#", 1), regex.Replace(springs.ToString(), ".", 1) };
-//         return permutations.Sum(x => CalculatePermutations(x, config));
-//     }
+        foreach (var item in items)
+        {
+            var count = BruteForce(item.Item1, item.Item2);
+            Console.WriteLine(count + " " + item.Item1);
+            answer += count;
+        }
+        return answer;
+    }
 
-//     private static (string, List<int>) RemoveCompletedBlocks(string springs, List<int> config)
-//     {
-//         var newConfig = new List<int>(config);
-//         while (true)
-//         {
-//             if (newConfig.Count == 0) break;
-//             var newSprings = springs;
-//             var leadingHeksjes = springs.IndexOfAny(['.', '?']);
-//             if (leadingHeksjes == newConfig[0])
-//             {
-//                 newSprings = springs[(leadingHeksjes + 1)..].Trim('.');
-//                 newConfig.RemoveAt(0);
-//             }
-//             if (newConfig.Count == 0) { springs = newSprings; break; }
-//             var trailingHeksjes = newSprings.Length - newSprings.LastIndexOfAny(['.', '?']);
-//             if (trailingHeksjes == newConfig[^1] + 1 && newSprings.LastIndexOfAny(['.', '?']) != -1)
-//             {
-//                 newSprings = newSprings[..^trailingHeksjes].Trim('.');
-//                 newConfig.RemoveAt(newConfig.Count - 1);
-//             }
+    private long BruteForce(string sections, List<int> groups)
+    {
+        var flex = Flexibility(sections, groups);
+        if (Cache.TryGetValue(CachKey(sections, groups), out long ans)) return ans;
 
-//             // Todo remove blocks in the middle
-//             if (newSprings == springs) { springs = newSprings; break; }
-//             springs = newSprings;
-//         }
+        if (!sections.Contains('?') || groups.Count == 0) return 1;
+        if (flex < 0 || (flex == 0 && NotPossible(sections, groups))) return 0;
+        var group = groups[0];
+        var totals = 0L;
+        var totalsSets = new List<(int, long)>();
+        for (int j = 0; j < flex + 1; j++)
+        {
+            if (j > 0 && '#' == sections[j - 1]) continue;
+            if (j + group < sections.Length && '#' == sections[j + group]) continue;
+            var upper = j + group;
+            if (sections[j..upper].Any(x => x == '.')) continue;
 
-//         return (springs, newConfig);
-//     }
+            if (j > 0 && SimpleInvalid(sections[..(j - 1)], groups.Take(0))) continue;
+            if (j + group < sections.Length && SimpleInvalid(sections[(upper + 1)..], groups.Skip(1))) continue;
+            var bStart = j + group + 1;
+            var secb = bStart >= sections.Length ? "" : sections[bStart..];
+            var combinations = BruteForce(Reduce(secb), groups.Skip(1).ToList()); ;
+            totalsSets.Add((j, combinations));
+            totals += combinations;
+        }
+        Cache[CachKey(sections, groups)] = totals;
+        return totals;
+    }
 
-//     private static string ReplaceSureFields(string springs, List<int> config, int totalHekje, int totalOpen, int totalVariable)
-//     {
-//         var newSprings = PlaceSureHekje(springs, config, totalHekje, totalOpen, totalVariable);
-//         newSprings = DueToImpossiblity(newSprings, config);
-//         return newSprings;
-//     }
+    private static string CachKey(string sections, List<int> groups)
+    => new string(sections) + " " + string.Join(",", groups);
 
-//     private static string PlaceSureHekje(string springs, List<int> config, int totalHekje, int totalOpen, int totalVariable)
-//     {
-//         var sureHekjes = config.Select(x => x - totalVariable).ToList();
-//         if (sureHekjes.All(x => x == 0)) return springs;
-//         var pos = 0;
-//         for (int i = 0; i < config.Count; i++)
-//         {
-//             var sure = sureHekjes[i];
-//             if (sure > 0)
-//             {
-//                 var updater = new StringBuilder(springs);
-//                 for (int j = pos + config[i] - sure; j < pos + config[i]; j++)
-//                 {
-//                     updater[j] = '#';
-//                 }
-//                 springs = updater.ToString();
-//             }
-//             pos += 1 + config[i];
-//         }
-//         return springs;
-//     }
 
-//     private static string DueToImpossiblity(string springs, List<int> config)
-//     {
-//         for (int i = 0; i < springs.Length; i++)
-//         {
-//             if (springs[i] != '?') continue;
-//             var possHekje = new StringBuilder(springs);
-//             possHekje[i] = '#';
-//             var possDot = new StringBuilder(springs);
-//             possDot[i] = '.';
-//             if (PrematureInvalid(possDot.ToString(), config)) springs = possHekje.ToString();
-//             else if (PrematureInvalid(possHekje.ToString(), config)) springs = possDot.ToString();
-//         }
-//         return springs;
-//     }
+    private static bool NotPossible(string sections, List<int> groups)
+    {
+        for (int i = 0; i < groups.Count; i++)
+        {
+            var group = groups[i];
+            var pos = groups.Take(i).Sum() + i;
+            if (pos > 0 && '#' == sections[pos - 1]) return true;
+            if (pos + group < sections.Length && '#' == sections[pos + group]) return true;
+            var upper = pos + group;
+            if (sections[pos..upper].Any(x => x == '.')) return true;
+        }
+        return false;
+    }
 
-//     private static bool PrematureInvalid(string springs, List<int> config)
-//     {
-//         if (springs.Count(x => x == '#') > config.Sum()) return true;
-//         if (springs.Count(x => x == '.') > springs.Length - config.Sum()) return true;
-//         var springCounts1 = springs.Split(new char[] { '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Length);
-//         var tooLong = springCounts1.Any(count => count > config.Max());
-//         // check if it contains any with a length that doesn't exist
-//         var springCounts2 = springs.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).Where(x => !x.Contains('?')).Select(x => x.Length);
-//         var nonExistant = springCounts2.Any(x => !config.Contains(x));
-//         return tooLong || nonExistant;
-//     }
+    private static bool SimpleInvalid(string sections, IEnumerable<int> groups)
+    {
+        if (!groups.Any()) return sections.Contains('#');
+        if (HasTooManyLargeGroups(sections, groups)) return true;
+        // trim . en check of lengte nog voldoende
+        return false;
+    }
 
-//     private static bool IsValid(string springs, List<int> config)
-//     {
-//         var springGroups = GetSpringGroups(springs);
-//         return springGroups == string.Join(",", config);
-//     }
+    private static bool HasTooManyLargeGroups(string sections, IEnumerable<int> groups)
+    {
+        var largestGroup = groups.Max();
+        var largestGroupCount = groups.Count(x => x == largestGroup);
+        var pos = 0;
+        var groupCount = 0;
+        var hekjeCounter = 0;
+        while (pos < sections.Length)
+        {
+            if (sections[pos] == '.' || sections[pos] == '?')
+            {
+                if (hekjeCounter > largestGroup) return true;
+                if (hekjeCounter == largestGroup) groupCount++;
+                hekjeCounter = 0;
+            }
+            if (sections[pos] == '#')
+            {
+                hekjeCounter++;
+            }
+            pos++;
+        }
+        if (hekjeCounter > largestGroup) return true;
+        if (hekjeCounter == largestGroup) groupCount++;
+        return groupCount > largestGroupCount;
+    }
 
-//     private static string GetSpringGroups(string springs)
-//         => string.Join(",", springs.ToString().Split('.').Where(x => x.Length != 0).Select(x => x.Length));
+    private static (string, List<int>) ParseInput(string line, bool timesFive)
+    {
+        var (sections, groups) = (line.Split(" ")[0], line.Split(" ")[1]);
+        if (timesFive)
+        {
+            var oldSections = sections;
+            var oldGroups = groups;
+            for (int i = 0; i < 4; i++)
+            {
+                sections += "?" + oldSections;
+                groups += "," + oldGroups;
+            }
+        }
+        return (Reduce(sections), groups.Split(",").Select(int.Parse).ToList());
+    }
 
-//     [Example(expected: 525152, input: "???.### 1,1,3\n.??..??...?##. 1,1,3\n?#?#?#?#?#?#?#? 1,3,1,6\n????.#...#... 4,1,1\n????.######..#####. 1,6,5\n?###???????? 3,2,1")]
-//     [Example(expected: 525152, input: ".??..??...?##. 1,1,3")]
-//     [Puzzle(expected: 222222)]
-//     public static int Part2(string input)
-//         => ReadLines(input).Select(Multiply).Sum(CalculatePermutations);
+    private static int Flexibility(string sections, List<int> groups)
+    => sections.Length - groups.Count - groups.Sum() + 1;
 
-//     private static string Multiply(string line)
-//     {
-//         var (springs, configuration) = (line.Split(" ")[0], line.Split(" ")[1]);
-//         return string.Concat(Enumerable.Repeat(springs + '?', 5))[..^1] + " " + string.Concat(Enumerable.Repeat(configuration + ',', 5))[..^1];
-//     }
-// }
+    public static string Reduce(string s)
+    {
+        s = s.Trim('.');
+        while (s.Contains("..")) s = s.Replace("..", ".");
+        return s;
+    }
+}
