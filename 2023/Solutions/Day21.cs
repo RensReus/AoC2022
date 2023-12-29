06b35215b601aa1d5358de928d49274142f3f682
Rens Reus <rensreus@gmail.com>
2023-12-29 18:16:24 +0100

    Day 21

--- 2023/Solutions/Day21.cs
+++ 2023/Solutions/Day21.cs
@@ -12,13 +12,13 @@ class Day21 : BaseDay
         return CountPossibleFields(startRow, startCol, lines, 64, true);
     }
 
-    private static long CountPossibleFields(int startRow, int startCol, List<string> lines, int limit, bool even, long lastStep = 0)
+    private static long CountPossibleFields(int startRow, int startCol, List<string> lines, int limit, bool even)
     {
         var visitedEven = new Dictionary<(int, int), bool>();
         var visitedOdd = new Dictionary<(int, int), bool>();
         var toEval = new List<(int, int)> { (startRow, startCol) };
         var size = lines.Count;
-        for (long i = 1 + lastStep; i <= limit; i++)
+        for (long i = 1; i <= limit; i++)
         {
             var nextToEval = new List<(int, int)>();
             foreach (var item in toEval)
@@ -40,14 +40,13 @@ class Day21 : BaseDay
             }
             toEval = nextToEval;
         }
-        Console.WriteLine(visitedEven.Last());
         return even ? visitedEven.Count : visitedOdd.Count;
     }
 
     private static List<(int, int)> GetNeighbours((int, int) item)
         => [(item.Item1 + 1, item.Item2), (item.Item1 - 1, item.Item2), (item.Item1, item.Item2 - 1), (item.Item1, item.Item2 + 1)];
 
-    [Puzzle(expected: 610275506007391)] // too high  609701953729856 // too low because missing non complete tiles
+    [Puzzle(expected: 609708004316870)]
     public static long Part2(string input)
     {
         var lines = ReadLines(input);
@@ -59,50 +58,64 @@ class Day21 : BaseDay
         var gridwidth = lines.Count;
         int totalGridSteps = targetSteps / gridwidth;
         int remainingSteps = targetSteps % gridwidth;
-        var totalOddGrids = 1L;
-        var totalEvenGrids = 0L;
-        Console.WriteLine(gridwidth);
-        Console.WriteLine(remainingSteps);
-        for (int i = 1; i < totalGridSteps; i++) // Kan met formule
+        var totalEvenGrids = 1L;
+        var totalOddGrids = 0L;
+        for (int i = 0; i < totalGridSteps; i++) // Kan wss met formule
         {
-            if (i % 2 == 0) totalOddGrids += i * 4;
-            else totalEvenGrids += i * 4;
+            if (i % 2 == 0) totalEvenGrids += i * 4;
+            else totalOddGrids += i * 4;
         }
-        var visitableInEvenGrid = CountPossibleFields(startRow, startCol, lines, gridwidth, true);
-        var visitableInOddGrid = CountPossibleFields(startRow, startCol, lines, gridwidth, false);
-        var countCompletedGrids = visitableInEvenGrid * totalOddGrids + visitableInOddGrid * totalEvenGrids;
+        var visitableInEvenGrid = CountPossibleFields(startRow, startCol, lines, gridwidth, false);
+        var visitableInOddGrid = CountPossibleFields(startRow, startCol, lines, gridwidth, true);
 
-        //var bonusSteps = remainingSteps + gridwidth / 2;
-        //var leftBonus = CountPossibleFields(startRow, 0, lines, bonusSteps, true);
-        //var rightBonus = CountPossibleFields(startRow, gridwidth, lines, bonusSteps, true);
-        //var topBonus = CountPossibleFields(0, startCol, lines, bonusSteps, true);
-        //var bottomBonus = CountPossibleFields(gridwidth, startCol, lines, bonusSteps, true);
-        //var topRight = CountPossibleFields(0, gridwidth, lines, remainingSteps, true);
-        //var topLeft = CountPossibleFields(0, 0, lines, remainingSteps, true);
-        //var bottomRight = CountPossibleFields(gridwidth, gridwidth, lines, remainingSteps, true);
-        //var bottomLeft = CountPossibleFields(gridwidth, 0, lines, remainingSteps, true);
+        var countCompletedGrids = visitableInEvenGrid * totalEvenGrids + visitableInOddGrid * totalOddGrids;
+
         return countCompletedGrids
-            + StraightBonus(targetSteps - remainingSteps, totalGridSteps % 2 == 0, startRow, startCol, lines, targetSteps, gridwidth)
-            + DiagonalBonus();
-        // + leftBonus + rightBonus + topBonus + bottomBonus
-        // + (topRight + topLeft + bottomRight + bottomLeft) * (totalGridSteps - 2);
+            + StraightBonus(remainingSteps, totalGridSteps % 2 == 0, startRow, startCol, lines, gridwidth)
+            + DiagonalBonus(gridwidth, lines, remainingSteps, totalGridSteps);
     }
 
-    private static long DiagonalBonus()
+    private static long DiagonalBonus(int gridwidth, List<string> lines, int remainingSteps, int totalGridSteps)
+        => DiagonalBonusEven(gridwidth, lines, remainingSteps - 1, totalGridSteps) + DiagonalBonusOdd(gridwidth, lines, remainingSteps, totalGridSteps);
+
+    private static long DiagonalBonusOdd(int gridwidth, List<string> lines, int remainingSteps, int totalGridSteps)
     {
-        // 2 soorten diagonals
-        throw new NotImplementedException();
+        var evenGridSteps = totalGridSteps % 2 == 0;
+        var bonusSteps = !evenGridSteps ? gridwidth : 0;
+        var actualRemainingSteps = remainingSteps + bonusSteps;
+        var topRight = CountPossibleFields(0, gridwidth - 1, lines, actualRemainingSteps, true);
+        var topLeft = CountPossibleFields(0, 0, lines, actualRemainingSteps, true);
+        var bottomRight = CountPossibleFields(gridwidth - 1, gridwidth - 1, lines, actualRemainingSteps, true);
+        var bottomLeft = CountPossibleFields(gridwidth - 1, 0, lines, actualRemainingSteps, true);
+
+        var oddDiagonalCount = !evenGridSteps ? totalGridSteps - 1 : totalGridSteps;
+
+        return (topRight + topLeft + bottomLeft + bottomRight) * oddDiagonalCount;
     }
 
-    private static long StraightBonus(long stepsMade, bool isEven, int startRow, int startCol, List<string> lines, int targetSteps, int gridwidth)
+    private static long DiagonalBonusEven(int gridwidth, List<string> lines, int remainingSteps, int totalGridSteps)
+    {
+        var evenGridSteps = totalGridSteps % 2 == 0;
+        var bonusSteps = evenGridSteps ? gridwidth : 0;
+        var actualRemainingSteps = remainingSteps + bonusSteps;
+        var topRight = CountPossibleFields(0, gridwidth - 1, lines, actualRemainingSteps, false);
+        var topLeft = CountPossibleFields(0, 0, lines, actualRemainingSteps, false);
+        var bottomRight = CountPossibleFields(gridwidth - 1, gridwidth - 1, lines, actualRemainingSteps, false);
+        var bottomLeft = CountPossibleFields(gridwidth - 1, 0, lines, actualRemainingSteps, false);
+
+        var evenDiagonalCount = evenGridSteps ? totalGridSteps - 1 : totalGridSteps;
+        return (topRight + topLeft + bottomLeft + bottomRight) * evenDiagonalCount;
+    }
+
+    private static long StraightBonus(int remainingSteps, bool isEven, int startRow, int startCol, List<string> lines, int gridwidth)
     {
         var hoeveelsnellerEigenlijk = gridwidth / 2;
-        var lastStep = stepsMade - hoeveelsnellerEigenlijk;
-        if (targetSteps - lastStep > gridwidth) throw new Exception("moet nog een 4 extra grids meepakken");
-        var leftBonus = CountPossibleFields(startRow, 0, lines, targetSteps, isEven, lastStep);
-        var rightBonus = CountPossibleFields(startRow, gridwidth, lines, targetSteps, isEven, lastStep);
-        var topBonus = CountPossibleFields(0, startCol, lines, targetSteps, isEven, lastStep);
-        var bottomBonus = CountPossibleFields(gridwidth, startCol, lines, targetSteps, isEven, lastStep);
+        var actualRemainingSteps = remainingSteps + hoeveelsnellerEigenlijk;
+        if (actualRemainingSteps >= gridwidth) throw new Exception("moet nog een 4 extra grids meepakken");
+        var leftBonus = CountPossibleFields(startRow, 0, lines, actualRemainingSteps, isEven);
+        var rightBonus = CountPossibleFields(startRow, gridwidth - 1, lines, actualRemainingSteps, isEven);
+        var topBonus = CountPossibleFields(0, startCol, lines, actualRemainingSteps, isEven);
+        var bottomBonus = CountPossibleFields(gridwidth - 1, startCol, lines, actualRemainingSteps, isEven);
 
         return leftBonus + rightBonus + topBonus + bottomBonus;
     }
