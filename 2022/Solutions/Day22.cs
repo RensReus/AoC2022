e71dd3a854365265a5602eeb5f8e414e159fd517
Rens Reus <rensreus@gmail.com>
2023-12-30 15:14:39 +0100

    2022 Finished

--- 2022/Solutions/Day22.cs
+++ 2022/Solutions/Day22.cs
@@ -2,14 +2,13 @@ namespace AoC2022;
 
 class Day22 : BaseDay
 {
-    static (char[,], List<Move>, int, int) ProcessInput(string input)
+    static (char[,], List<Move>, int) ProcessInput(string input)
     {
         var forestlines = input.Split("\n\n")[0].Split("\n").ToList();
         var steps = input.Split("\n\n")[1];
         var width = forestlines.Max(x => x.Length);
         var height = forestlines.Count;
         var forest = new char[width, height];
-        var faceSize = int.MaxValue;
         for (int row = 0; row < height; row++)
         {
             var rowFaceSize = 0;
@@ -20,10 +19,9 @@ class Day22 : BaseDay
                 forest[col, row] = line[col];
                 if (line[col] == '.' || line[col] == '#') rowFaceSize++;
             }
-            faceSize = int.Min(faceSize, rowFaceSize);
         }
         var moves = SplitMoveString(steps);
-        return (forest, moves, forestlines[0].IndexOf('.'), faceSize);
+        return (forest, moves, forestlines[0].IndexOf('.'));
     }
 
     private static List<Move> SplitMoveString(string steps)
@@ -51,7 +49,7 @@ class Day22 : BaseDay
     [Puzzle(expected: 97356)]
     public static int Part1(string input)
     {
-        (var forest, var moves, var startingCol, _) = ProcessInput(input);
+        (var forest, var moves, var startingCol) = ProcessInput(input);
         var maxCol = forest.GetLength(0);
         var maxRow = forest.GetLength(1);
         var position = new Position(startingCol);
@@ -59,7 +57,7 @@ class Day22 : BaseDay
         {
             if (move.RotationMove)
             {
-                position.Direction.Rotate(move.RotationDirection);
+                position.DirectionAttr.Rotate(move.RotationDirection);
             }
             else
             {
@@ -80,18 +78,16 @@ class Day22 : BaseDay
         return position.Answer();
     }
 
-    [Example(expected: 5031, input: "        ...#\n        .#..\n        #...\n        ....\n...#.......#\n........#...\n..#....#....\n..........#.\n        ...#....\n        .....#..\n        .#......\n        ......#.\n\n10R5L5R10L4R5L5")]
-    [Puzzle(expected: 222222)]
+    [Puzzle(expected: 120175)]
     public static int Part2(string input)
     {
-        (var forest, var moves, var startingCol, var faceSize) = ProcessInput(input);
-        var faces = ExtractFaces(forest, faceSize);
+        (var forest, var moves, var startingCol) = ProcessInput(input);
         var position = new Position2(startingCol);
         foreach (var move in moves)
         {
             if (move.RotationMove)
             {
-                position.Rotate(move.RotationDirection);
+                position.DirectionAttr.Rotate(move.RotationDirection);
             }
             else
             {
@@ -99,11 +95,11 @@ class Day22 : BaseDay
                 var stepsTaken = 0;
                 while (stepsTaken < move.MoveLength)
                 {
-                    (var nextCol, var nextRow, var newFace, var newDir, var canMove) = position.NexPos(forest, faces);
+                    (var nextCol, var nextRow, var newDir, var canMove) = position.NexPos(forest);
                     if (canMove)
                     {
                         stepsTaken++;
-                        position.UpdatePosition(nextCol, nextRow, newFace, newDir);
+                        position.UpdatePosition(nextCol, nextRow, newDir);
                     }
                     else break;
                 }
@@ -111,170 +107,81 @@ class Day22 : BaseDay
         }
         return position.Answer();
     }
-
-    private static List<Face> ExtractFaces(char[,] forest, int faceSize)
-    {
-        var faces = new List<Face>();
-        for (int row = 0; row < forest.GetLength(1) / faceSize; row++)
-        {
-            for (int col = 0; col < forest.GetLength(0) / faceSize; col++)
-            {
-                if (".#".Contains(forest[col * faceSize, row * faceSize]))
-                {
-                    faces.Add(new(col, row, faceSize));
-                }
-            }
-        }
-        return faces;
-    }
-}
-
-internal class Face
-{
-    public int MinCol => Col * FaceSize;
-    public int MaxCol => MinCol + FaceSize - 1;
-    public int MinRow => Row * FaceSize;
-    public int MaxRow => MinRow + FaceSize - 1;
-    public int Row;
-    public int Col;
-    public int FaceSize;
-
-    public Face(int col, int row, int faceSize)
-    {
-        Row = row;
-        Col = col;
-        FaceSize = faceSize;
-    }
-}
-
-public class Neighbour
-{
-    public int Face;
-    public int Rotation; // 0, 1, 2, 3 clockwise
-    public char DestSide;
-
-    public Neighbour(string input)
-    {
-        Face = input[1] - '1';
-        Rotation = GetRotation(input[0], input[2]);
-        DestSide = input[2];
-    }
-
-    private int GetRotation(char sourceSide, char destinationSide)
-    {
-        var sides = new List<char> { 'U', 'R', 'D', 'L' };
-        return (sides.IndexOf(sourceSide) - sides.IndexOf(destinationSide) + 4 + 2) % 4;
-    }
 }
 
 internal class Position2
 {
     public int Col;
     public int Row;
-    public int Face;
-    public int Direction;
-    public Position2(int startingCol)
+    public Direction DirectionAttr;
+
+    public Position2(int startingCol, int startRow = 0)
     {
         Col = startingCol;
+        Row = 0;
+        DirectionAttr = Direction.East;
     }
 
     internal int Answer()
-        => (Row + 1) * 1000 + (Col + 1) * 4 + Direction;
+        => (Row + 1) * 1000 + (Col + 1) * 4 + DirectionAttr.Answer();
 
-    internal void UpdatePosition(int newCol, int newRow, int newFace, int newDir)
+    internal void UpdatePosition(int newCol, int newRow, Direction newDir)
     {
         Col = newCol;
         Row = newRow;
-        Face = newFace;
-        Direction = newDir;
+        DirectionAttr = newDir;
     }
 
-    internal (int, int, int, int, bool) NexPos(char[,] forest, List<Face> faces)
+    internal (int, int, Direction, bool) NexPos(char[,] forest)
     {
-        var newCol = Col + Delta().Item1;
-        var newRow = Row + Delta().Item2;
-        var currentFace = faces[Face];
-        (newCol, newRow, int newDir, int newFace) = (newCol, newRow) switch
-        {
-            (_, _) when newCol > currentFace.MaxCol => ApplyTranslation(newCol, newRow, 'R'),
-            (_, _) when newRow > currentFace.MaxRow => ApplyTranslation(newCol, newRow, 'D'),
-            (_, _) when newCol < currentFace.MinCol => ApplyTranslation(newCol, newRow, 'L'),
-            (_, _) when newRow < currentFace.MinRow => ApplyTranslation(newCol, newRow, 'U'),
-            (_, _) => (newCol, newRow, Direction, Face)
-        };
-        return (newCol, newRow, newFace, newDir, forest[newCol, newRow] is '.');
+        var newCol = Col + DirectionAttr.DeltaCol;
+        var newRow = Row + DirectionAttr.DeltaRow;
+        var newDir = DirectionAttr;
+        if (OutOfBounds(newRow, newCol, forest) || forest[newCol, newRow] == ' ' || forest[newCol, newRow] == 0) (newRow, newCol, newDir) = FaceJump(newRow, newCol);
+        return (newCol, newRow, newDir, forest[newCol, newRow] == '.');
     }
 
-    private (int, int) Delta()
-        => (Direction) switch
-        {
-            0 => (1, 0),
-            1 => (0, 1),
-            2 => (-1, 0),
-            3 => (0, -1),
-            _ => throw new InvalidDataException()
-        };
-
-    private (int newCol, int newRow, int newDir, int newFace) ApplyTranslation(int newCol, int newRow, char source)
-        => (Face, source) switch
-        {
-            (0, 'D') => (newCol, newRow, Direction, 2),
-            (0, 'R') => (newCol, newRow, Direction, 1),
-            (1, 'L') => (newCol, newRow, Direction, 0),
-            (2, 'U') => (newCol, newRow, Direction, 0),
-            (2, 'D') => (newCol, newRow, Direction, 4),
-            (3, 'R') => (newCol, newRow, Direction, 4),
-            (3, 'D') => (newCol, newRow, Direction, 5),
-            (4, 'U') => (newCol, newRow, Direction, 2),
-            (4, 'L') => (newCol, newRow, Direction, 3),
-            (5, 'U') => (newCol, newRow, Direction, 3),
-            _ => ComplexereTransitie(newCol, newRow, source)
-        };
-
-    private (int newCol, int newRow, int Direction, int) ComplexereTransitie(int newCol, int newRow, char source)
-        => (Face, source) switch
-        {
-            //(0, 'L') => (Col - 50, FlipCoord(newRow - currface.MinCol) - newFace.MinRow, 0, 5), // Flipcoord moet met relative coords
-            (1, 'R') => (Col - 50, FlipCoord(newRow) - 100, 2, 4),
-            (1, 'U') => (Col - 100, newRow - 150, Direction, 5),
-            (3, 'L') => (Col + 50, FlipCoord(newRow) + 100, 0, 0),
-            (4, 'R') => (Col + 50, FlipCoord(newRow) + 100, 2, 1),
-            (5, 'D') => (Col + 50, newRow + 150, Direction, 1),
-            _ => NogComplexer(newCol, newRow, source)
-        };
-
-
-    private (int, int, int, int) NogComplexer(int newCol, int newRow, char source)
-        => (Face, source) switch
-        {
-            (0, 'U') => (Row, FlipCoord(Col) + 100, 0, 5),
-            (1, 'D') => (Row + 50, FlipCoord(Col) - 50, 2, 2),
-            (2, 'R') => (FlipCoord(Row), 50, 3, 1),
-            _ => throw new ArgumentOutOfRangeException()
-        };
-
-    private int FlipCoord(int oldCoord)
+    private bool OutOfBounds(int newRow, int newCol, char[,] forest)
     {
-        return 49 - oldCoord;
+        return newRow < 0 || newRow >= 200 || newCol < 0 || newCol >= 150;
     }
 
-    internal void Rotate(char rotationDirection)
-        => Direction = (Direction + 4 + (rotationDirection == 'R' ? 1 : -1)) % 4;
+    private static (int newRow, int newCol, Direction newDir) FaceJump(int newRow, int newCol)
+    {
+        return (newRow, newCol) switch
+        {
+            (-1, >= 50 and < 100) => (newCol + 100, 0, Direction.East),
+            (-1, >= 100 and < 150) => (199, newCol - 100, Direction.North),
+            ( >= 0 and < 50, 150) => (149 - newRow, 99, Direction.West),
+            (50, >= 100 and < 150) => (newCol - 50, 99, Direction.West),
+            ( >= 50 and < 100, 100) => (49, newRow + 50, Direction.North),
+            ( >= 100 and < 150, 100) => (49 - newRow + 100, 149, Direction.West),
+            (150, >= 50 and < 100) => (newCol + 100, 49, Direction.West),
+            ( >= 150 and < 200, 50) => (149, newRow - 100, Direction.North),
+            (200, >= 0 and < 50) => (0, newCol + 100, Direction.South),
+            ( >= 150 and < 200, -1) => (0, newRow - 100, Direction.South),
+            ( >= 100 and < 150, -1) => (49 - newRow + 100, 50, Direction.East),
+            (99, >= 0 and < 50) => (newCol + 50, 50, Direction.East),
+            ( >= 50 and < 100, 49) => (100, newRow - 50, Direction.South),
+            ( >= 0 and < 50, 49) => (149 - newRow, 0, Direction.East)
+        };
+    }
 }
 
 internal class Position
 {
     public int Col;
     public int Row;
-    public Direction Direction;
+    public Direction DirectionAttr;
+
     public Position(int startingCol)
     {
         Col = startingCol;
-        Direction = new Direction(1, 0);
+        DirectionAttr = new Direction(1, 0);
     }
 
     internal int Answer()
-        => (Row + 1) * 1000 + (Col + 1) * 4 + Direction.Answer();
+        => (Row + 1) * 1000 + (Col + 1) * 4 + DirectionAttr.Answer();
 
     internal void UpdatePosition(int newCol, int newRow)
     {
@@ -284,12 +191,12 @@ internal class Position
 
     internal (int, int, bool) NexPos(int maxCol, int maxRow, char[,] forest)
     {
-        var newCol = (Col + Direction.DeltaCol + maxCol) % maxCol;
-        var newRow = (Row + Direction.DeltaRow + maxRow) % maxRow;
+        var newCol = (Col + DirectionAttr.DeltaCol + maxCol) % maxCol;
+        var newRow = (Row + DirectionAttr.DeltaRow + maxRow) % maxRow;
         while (forest[newCol, newRow] is not '#' and not '.')
         {
-            newCol = (newCol + Direction.DeltaCol + maxCol) % maxCol;
-            newRow = (newRow + Direction.DeltaRow + maxRow) % maxRow;
+            newCol = (newCol + DirectionAttr.DeltaCol + maxCol) % maxCol;
+            newRow = (newRow + DirectionAttr.DeltaRow + maxRow) % maxRow;
         }
         return (newCol, newRow, forest[newCol, newRow] is '.');
     }
@@ -300,11 +207,12 @@ public class Direction
     public int DeltaCol;
     public int DeltaRow;
 
-    public Direction(int d1, int d2)
+    public Direction(int dCol, int dRow)
     {
-        DeltaCol = d1;
-        DeltaRow = d2;
+        DeltaCol = dCol;
+        DeltaRow = dRow;
     }
+
     public void Rotate(char c)
     {
         var oldCol = DeltaCol;
@@ -328,6 +236,11 @@ public class Direction
             (0, -1) => 3,
             _ => throw new InvalidDataException()
         };
+
+    public static Direction North => new(0, -1);
+    public static Direction South => new(0, 1);
+    public static Direction West => new(-1, 0);
+    public static Direction East => new(1, 0);
 }
 
 internal class Move
